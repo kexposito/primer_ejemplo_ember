7455e54b8c33a9a16bd5a7a522c3a5b4a3285acf
kevin <kevin@mimiquate.com>
2018-11-22 16:40:59 -0300

    modificacion de estilo titulo y listado application.hbs(p.37)

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,22 +1,30 @@
 //voy a controlar application hbs
 import Route from '@ember/routing/route';
 import EmberObject from '@ember/object';
+// la saco de emberObject y defino
+let Song=EmberObject.extend({
+    title:'',
+    band:'',
+    rating:0
+});
 
 export default Route.extend({
     model(){
-        let blackDog=EmberObject.create({
+
+        //patron de arriba
+        let blackDog=Song.create({
             title:'Black Dog',
             band:'Led Zeppelin',
             rating:3
         });
 
-        let yellowLedbetter=EmberObject.create({
+        let yellowLedbetter=Song.create({
             title:'Yellow Ledbetter',
             band:'Pearl Jam',
             rating:4
         });
 
-        let pretender=EmberObject.create({
+        let pretender=Song.create({
             title:'The Pretender',
             band:'Foo Fighters',
             rating:2
