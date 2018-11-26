dea31436b18f71d36b76bd27ce2eb521bb1ea5e5
kevin <kevin@mimiquate.com>
2018-11-26 09:49:13 -0300

    (p.145 andando. se agrego ventana emergente a details en caso de que el usuario quiera irse o cerrar la ventana sin terminar su edicion)

--- app/controllers/bands/band/details.js
+++ app/controllers/bands/band/details.js
@@ -1,4 +1,12 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    isEditing: false,
+
+    actiones:{
+        toggleIsEditing() {
+            this.toggleProperty('isEditing');
+
+        }
+    }
 });
