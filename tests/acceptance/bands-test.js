d6532fbcf4bdd418db787e73c22810cb5e12c6bf
kevin <kevin@mimiquate.com>
2018-11-27 15:49:48 -0300

    se incorpora los cambios FORMA1 y FORMA2 en test/acceptance/band-test. Se importa los custom-helpers y se aplican los cambios (p.185)

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { visit, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { createBand } from 'rarwe/tests/helpers/custom-helpers';
 
 module('Acceptance | Bands', function(hooks) {
   
@@ -17,7 +18,9 @@ module('Acceptance | Bands', function(hooks) {
     server.create('band', { name: 'Radiohead' });
     server.create('band', { name: 'Long Distance Calling'});
     await visit ('/bands');
-    /*let bandLinks=this.element.querySelectorAll('[data-test-band-link]');
+    /*
+    forma 1
+    let bandLinks=this.element.querySelectorAll('[data-test-band-link]');
     assert.equal(bandLinks.length, 2,"All band links are rendered");
     assert.ok(bandLinks[0].textContent.includes('Radiohead'),'First band link contains the name');
     assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'),'The other band link contains the bad name'); 
@@ -31,12 +34,19 @@ module('Acceptance | Bands', function(hooks) {
 
     server.create('band', { name: 'Royal Blood'  });
     await visit('/bands');
+    /* cambio 2 
     await click('[data-test-new-band-label]');
     await fillIn('[data-test-new-band-input]', 'Caspian'); 
     await click('[data-test-new-band-button]');
-
+    */
+    await createBand('Caspian');
+    
+    /*
+    forma 1
     let bandLinks = this.element.querySelectorAll('[data-test-band-link]');
-    /*assert.equal(bandLinks.length, 2 , 'All band links are rendered', 'A new band link is rendered');
+    
+    
+    assert.equal(bandLinks.length, 2 , 'All band links are rendered', 'A new band link is rendered');
     assert.ok(bandLinks[1].textContent.includes('Caspian'), 'The new band link is rendered');
    // assert.ok(this.element.querySelector('[data-test-band-nav-item] > .active').textContent.includes('Songs'), 'The Songs tab is active');*/
    assert.dom('[data-test-band-list-item]').exists({ count: 2 }, 'A new band link is rendered');
