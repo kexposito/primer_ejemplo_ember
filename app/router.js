94726f8eccf3d9c97d8a4b93a31633beed84f7cf
kevin <kevin@mimiquate.com>
2018-11-28 16:24:28 -0300

    finalizado

--- app/router.js
+++ app/router.js
@@ -7,8 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('bands', function() {//{ path: '/'}
-      this.route('band', { path: ':id'}, function() {   
+  this.route('not-found', { path: '/*path' }); // a partir de dodne quiero que pase esoto
+  this.route('bands', function() {//{ path: '/'} //agrego a que rutas puedo acceder   ///aca voy a tener un {{outlet}} que me lleva al template(.hbs) de band
+      this.route('band', { path: ':id'}, function() {    // el template de aca tiene dos div, uno con los link y el otro con un  {{otulet}} que me lleva el template (.hbs) de songs o de detail dependiendo lo que elija
         this.route('songs');
         this.route('details');
       });
